d467a52f947bec95b4b754b5c735a15167786cb3
Asim-afk <casimon8@gmail.com>
2022-11-09 11:35:01 +0545

    ok

---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Login-mitho-sekuwa</title>
+</head>
+<body>
+    <div class="login">
+        <h1>login</h1>
+        <p>
+            Designed by <a href="">Asim Chaulagain</a>
+        </p>
+    </div>
+</body>
+</html>
